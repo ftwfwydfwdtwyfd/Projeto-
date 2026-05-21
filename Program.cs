da3b261d07b96a728d47ef3860861ec6b88bec01
Viviane Maria de Lima Almeida <contatovivianemaria@gmail.com>
2026-05-20 21:15:19 -0300

    Dados Atualizados

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using ExoApi.Contexts;
 using ExoApi.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,9 +15,44 @@ builder.Services.AddTransient<ProjetoRepository, ProjetoRepository>();
 
 builder.Services.AddTransient<UsuarioRepository, UsuarioRepository>();  
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = "JwtBearer";
+    options.DefaultChallengeScheme = "JwtBearer";
+})
+.AddJwtBearer("JwtBearer", options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("exoapi-chave-autenticacao-senai-123")),
+        ClockSkew = TimeSpan.FromMinutes(30),
+        ValidIssuer = "exoapi.webapi",
+        ValidAudience = "exoapi.webapi"
+    };
+});
+
 var app = builder.Build();
 
 app.UseRouting();
+
+app.UseCors("CorsPolity");
+
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 app.UseEndpoints(endpoints =>
 {
